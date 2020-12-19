e43d01a896ebd2d0aad50d866142ebec41d6c945
Irina <irina.stoica@vanillaforums.com>
2020-12-19 10:50:25 -0500

    Clean Up Code

--- src/index.js
+++ src/index.js
@@ -12,20 +12,11 @@ import './index.scss';
 // Home layout
 import Demo from './page-demo/Demo';
 import MainDemo from './home/MainDemo';
-import Startup from './home/Startup';
-import Paralax from './home/Paralax';
 import HomePortfolio from './home/HomePortfolio';
-import DigitalAgency from './home/DigitalAgency';
-import CreativeAgency from './home/CreativeAgency';
 import PersonalPortfolio from './home/PersonalPortfolio';
-import Business from './home/Business';
-import StudioAgency from './home/StudioAgency';
 import PortfolioLanding from './home/PortfolioLanding';
-import CreativeLanding from './home/CreativeLanding';
 import HomeParticles from './home/HomeParticles';
-import CreativePortfolio from './home/CreativePortfolio';
 import InteriorLanding from './home/Interior';
-import CorporateBusiness from './home/CorporateBusiness';
 
 // Dark Home Layout 
 import DarkMainDemo from './dark/MainDemo';
@@ -69,22 +60,12 @@ class Root extends Component{
                     <Route exact path={`${process.env.PUBLIC_URL}/`} component={Demo}/>
                     <Route exact path={`${process.env.PUBLIC_URL}/main-demo`} component={MainDemo}/>
                     <Route exact path={`${process.env.PUBLIC_URL}/dark-main-demo`} component={DarkMainDemo}/>
-                    <Route exact path={`${process.env.PUBLIC_URL}/startup`} component={Startup}/>
-                    <Route exact path={`${process.env.PUBLIC_URL}/paralax`} component={Paralax}/>
-
-                    <Route exact path={`${process.env.PUBLIC_URL}/digital-agency`} component={DigitalAgency}/>
-                    <Route exact path={`${process.env.PUBLIC_URL}/creative-agency`} component={CreativeAgency}/>
                     <Route exact path={`${process.env.PUBLIC_URL}/personal-portfolio`} component={PersonalPortfolio}/>
-                    <Route exact path={`${process.env.PUBLIC_URL}/studio-agency`} component={StudioAgency}/>
-                    <Route exact path={`${process.env.PUBLIC_URL}/business`} component={Business}/>
                     <Route exact path={`${process.env.PUBLIC_URL}/portfolio-home`} component={HomePortfolio}/>
                     <Route exact path={`${process.env.PUBLIC_URL}/portfolio-landing`} component={PortfolioLanding}/>
-                    <Route exact path={`${process.env.PUBLIC_URL}/creative-landing`} component={CreativeLanding}/>
                     <Route exact path={`${process.env.PUBLIC_URL}/home-particles`} component={HomeParticles}/>
                     <Route exact path={`${process.env.PUBLIC_URL}/dark-portfolio-landing`} component={DarkPortfolioLanding}/>
-                    <Route exact path={`${process.env.PUBLIC_URL}/creative-portfolio`} component={CreativePortfolio}/>
                     <Route exact path={`${process.env.PUBLIC_URL}/interior-landing`} component={InteriorLanding}/>
-                    <Route exact path={`${process.env.PUBLIC_URL}/corporate-business`} component={CorporateBusiness}/>
 
 
                     {/* Element Layot */}
