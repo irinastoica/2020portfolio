bd38b8157a665cf3349c5882dde950885c6a3c38
Irina <irina.stoica@vanillaforums.com>
2021-03-10 21:52:22 -0500

    Finish Page

--- src/elements/PortfolioVanillaConversations.jsx
+++ src/elements/PortfolioVanillaConversations.jsx
@@ -6,10 +6,9 @@ import { FiChevronUp } from "react-icons/fi";
 import Header from "../component/header/HeaderDetails";
 import Footer from "../component/footer/Footer";
 
-import {TweenMax, TimelineLite, Power3} from 'gsap'
 
 //Assets
-import arrow from '../images/arrow-right.svg'
+import { Player } from "video-react";
 
 const SocialShare = [
   {Social: <FaFacebookF /> , link: 'https://www.facebook.com/'},
@@ -23,26 +22,6 @@ function PortfolioVanillaConversations() {
   let app = useRef(null);
   // let images =useRef(null);
   let content = useRef(null);
-  let tl = new TimelineLite({delay: .8})
-
-  useEffect( () => {
-
-    //Content Vars
-    const headlineFirst = content.children[0].children[0];
-    const headlineSecond = headlineFirst.nextSibling;
-    const contentP = content.children[1];
-    const contentButton = content.children[2];
-
-    // //Content Animation
-    tl.staggerFrom([headlineFirst.children, headlineSecond.children], 1, {
-      y: 20,
-      ease: Power3.easeOut,
-      delay: .4
-    }, .15, 'Start')
-      .from(contentP, 1, { y:20, opacity: 0, ease: Power3.easeOut}, 1.4)
-      .from(contentButton, 1, { y:20, opacity: 0, ease: Power3.easeOut}, 1.6)
-
-  }, [tl])
 
   return(
     <React.Fragment>
@@ -56,17 +35,25 @@ function PortfolioVanillaConversations() {
         <div className='containerApp'>
         </div>
       </div>
-       <div className="details-banner pt--120 pb--190"  data-black-overlay="7" ref={el => content = el}>
-         <div className="container-inner topDescription col-4">
-             <h2>Communications by Vanilla Event </h2>
-             <div className="description">
+       <div className="details-banner defaultMargin"  data-black-overlay="7" ref={el => content = el}>
+         <div className="container-inner topDescription col-lg-5 col-md-12 animatedParent">
+             <h2 className="animated fadeInUp">Communications by Vanilla Event </h2>
+             <div className="description animated fadeInUp">
                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed risus vel elit dapibus mollis.
                  Quisque lobortis dolor leo, quis vulputate nisl tempor vel. Mauris vel commodo est. Nunc semper scelerisque
                  lacus at laoreet. Quisque scelerisque volutpat massa vel porttitor. Nam laoreet nibh sit amet ligula
                  condimentum, a viverra leo gravida. Vestibulum ut dolor varius, varius turpis nec, condimentum sapien.</p>
              </div>
          </div>
-         <div className="introImage">
+         <div className="introImage col-xl-7 col-lg-7 col-md-12">
+           <div className="image-boxShadow">
+             <Player
+               autoPlay
+               loop
+               muted
+               poster="/assets/poster.png"
+               src={`../assets/videos/conversations.mp4`} />
+           </div>
          </div>
        </div>
       {/* End Breadcrump Area */}
@@ -74,25 +61,29 @@ function PortfolioVanillaConversations() {
       {/* Start Portfolio Details */}
       <div className="rn-portfolio-details bg_color--1">
             <div className="portfolio-details">
-                <div className="thumb mb--30 fadeInDown"  data-scroll-speed="15">
+                <div className="thumb mb--30"  data-scroll-speed="10">
                   <img src="/assets/images/portfolio/conversations-1.jpg" className="fullWidth" alt="Portfolio Images"/>
                 </div>
 
                 <div className="container defaultTopMargin content">
-                  <div className="thumb mb--30 box fadeInDown" data-scroll-speed="14">
-                    <img src="/assets/images/portfolio/conversations-2.png"  className="image-boxShadow" alt="Portfolio Images"/>
+                  <div className="thumb mb--30 box" data-scroll-speed="8">
+                    <img src="/assets/images/portfolio/conversations-2.png"  className="image-boxShadow singlePhoto" alt="Portfolio Images"/>
                   </div>
                 </div>
 
                 <div className="images-column-2 content">
                   <div className="wrapper">
-                    <div className="thumb mb--30 box mr--40 fadeInDown" data-scroll-speed="-8">
-                      <img src="/assets/images/portfolio/conversations-3a.png" className="fullWidth fadeInDown image-boxShadow" alt="Portfolio Images"/>
+                    <div className="thumb mb--30 box mr--40 ">
+                      <img src="/assets/images/portfolio/conversations-3a.png" className="fullWidth image-boxShadow" alt="Portfolio Images"/>
                     </div>
-                    <div className="thumb mb--30 box fadeInDown" data-scroll-speed="-16">
+                    <div className="thumb mb--30 box" data-scroll-speed="16">
                       <img src="/assets/images/portfolio/conversations-3b.png" className="fullWidth image-boxShadow" alt="Portfolio Images"/>
                     </div>
                   </div>
+                  <div className="backgroundColor"></div>
+              </div>
+              <div className="thumb mb--30 box last small" data-scroll-speed="14">
+                <img src="/assets/images/portfolio/conversations-3c.png" className="fullWidth" alt="Portfolio Images"/>
               </div>
             </div>
       </div>
